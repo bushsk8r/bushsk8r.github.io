246b6c1129711faa2849bd9f36a2c47c1eb67683
bushsk8r <msagajustin@gmail.com>
2022-03-24 12:25:24 -0700

    echo chamber playlist update

---
+++ audio_player.js
@@ -0,0 +1,47 @@
+function audioPlayer(){
+           var currentSong = 0;
+           $("#audioPlayer")[0].src = $("#start li a")[0];
+           $("#audioPlayer")[0].play();
+           $("#start li a").click(function(e){
+              e.preventDefault();
+              $("#audioPlayer")[0].src = this;
+              $("#audioPlayer")[0].play();
+              $("#start li").removeClass("current-song");
+               currentSong = $(this).parent().index();
+               $(this).parent().addClass("current-song");
+           });
+
+           $("#audioPlayer")[0].addEventListener("ended", function(){
+              currentSong++;
+               if(currentSong == $("#start li a").length)
+                   currentSong = 0;
+               $("#start li").removeClass("current-song");
+               $("#start li:eq("+currentSong+")").addClass("current-song");
+               $("#audioPlayer")[0].src = $("#start li a")[currentSong].href;
+               $("#audioPlayer")[0].play();
+           });
+       }
+
+function audioPlayer_echo(){
+            var currentSong = 0;
+              $("#audioPlayer")[0].src = $("#echo_chamber li a")[0];
+             // $("#audioPlayer")[0].play();
+              $("#echo_chamber li a").click(function(e){
+                 e.preventDefault();
+                 $("#audioPlayer")[0].src = this;
+                 $("#audioPlayer")[0].play();
+                 $("#echo_chamber li").removeClass("current-song");
+                  currentSong = $(this).parent().index();
+                  $(this).parent().addClass("current-song");
+                 });
+
+                 $("#audioPlayer")[0].addEventListener("ended", function(){
+                 currentSong++;
+                  if(currentSong == $("#echo_chamber li a").length)
+                      currentSong = 0;
+                  $("#echo_chamber li").removeClass("current-song");
+                  $("#echo_chamber li:eq("+currentSong+")").addClass("current-song");
+                  $("#audioPlayer")[0].src = $("#echo_chamber li a")[currentSong].href;
+                  $("#audioPlayer")[0].play();
+                 });
+             }
