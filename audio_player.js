bd40c8e9d75997af9dfaf8fa438686ff4196c372
bushsk8r <msagajustin@gmail.com>
2022-03-24 12:33:39 -0700

    player update

--- audio_player.js
+++ audio_player.js
@@ -24,24 +24,24 @@ function audioPlayer(){
 
 function audioPlayer_echo(){
             var currentSong = 0;
-              $("#audioPlayer")[0].src = $("#echo_chamber li a")[0];
+              $("#audioPlayer_echo")[0].src = $("#echo_chamber li a")[0];
              // $("#audioPlayer")[0].play();
               $("#echo_chamber li a").click(function(e){
                  e.preventDefault();
-                 $("#audioPlayer")[0].src = this;
-                 $("#audioPlayer")[0].play();
+                 $("#audioPlayer_echo")[0].src = this;
+                 $("#audioPlayer_echo")[0].play();
                  $("#echo_chamber li").removeClass("current-song");
                   currentSong = $(this).parent().index();
                   $(this).parent().addClass("current-song");
                  });
 
-                 $("#audioPlayer")[0].addEventListener("ended", function(){
+                 $("#audioPlayer_echo")[0].addEventListener("ended", function(){
                  currentSong++;
                   if(currentSong == $("#echo_chamber li a").length)
                       currentSong = 0;
                   $("#echo_chamber li").removeClass("current-song");
                   $("#echo_chamber li:eq("+currentSong+")").addClass("current-song");
-                  $("#audioPlayer")[0].src = $("#echo_chamber li a")[currentSong].href;
-                  $("#audioPlayer")[0].play();
+                  $("#audioPlayer_echo")[0].src = $("#echo_chamber li a")[currentSong].href;
+                  $("#audioPlayer_echo")[0].play();
                  });
              }
